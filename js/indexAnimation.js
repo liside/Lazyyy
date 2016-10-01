5beaf0d87d96f585c171fd10def39e0753d225fc
Xavier Xinwei Wang <xiw180@ucsd.edu>
2016-10-01 15:18:44 -0700

    front end font change

--- js/indexAnimation.js
+++ js/indexAnimation.js
@@ -6,13 +6,17 @@ $('#tight').click(function()
     $('#header').css("background-image", "url(images/tight.jpg)");
     $('#header').css("background-position-y", "-12vw");
     $("#toolbar").attr('class', 'mdl-layout__tab-bar mdl-js-ripple-effect mdl-color--primary-dark red');
+    $("#query").attr('class', 'large material-icons red');
+    $("#help").attr('class', 'mdl-cell--3-col btn-large red');
 });
 
 $('#slacky').click(function()
 {
     $('#header').css("background-image", "url(images/lazycover.jpg)");
     $('#header').css("background-position-y", "-27vw");
-    $("#toolbar").attr('class', 'mdl-layout__tab-bar mdl-js-ripple-effect mdl-color--primary-dark lime  lighten-2');
+    $("#toolbar").attr('class', 'mdl-layout__tab-bar mdl-js-ripple-effect mdl-color--primary-dark lime lighten-2');
+    $("#query").attr('class', 'large material-icons green');
+    $("#help").attr('class', 'mdl-cell--3-col btn-large lime lighten-2');
 });
 
 $('#mediocre').click(function()
@@ -20,5 +24,7 @@ $('#mediocre').click(function()
     $('#header').css("background-image", "url(images/cover.jpg)");
     $('#header').css("background-position-y", "-25vw");
     $("#toolbar").attr('class', 'mdl-layout__tab-bar mdl-js-ripple-effect mdl-color--primary-dark orange accent-2');
+    $("#query").attr('class', 'large material-icons yellow darken-1');
+    $("#help").attr('class', 'mdl-cell--3-col btn-large orange accent-2');
 });
 
